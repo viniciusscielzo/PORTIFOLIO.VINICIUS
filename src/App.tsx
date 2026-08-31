46561097878d8bd8e545e95232f3febe060eb7fa
viniciusscielzo <vinicius.scielzo1@gmail.com>
2026-08-31 20:48:10 -0300

    Alteração /investimento

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import Home from "./Home";
 import Projects from "./Projects";
+import Investment from "./Investment";
 
 export default function App() {
-  return window.location.pathname.startsWith("/projetos") ? <Projects /> : <Home />;
+  if (window.location.pathname.startsWith("/projetos")) return <Projects />;
+  if (window.location.pathname.startsWith("/investimento")) return <Investment />;
+  return <Home />;
 }
